d6cfa855616e8d630e5f8829f82c94268ad727f8
RISHII <rishikesh.palande07@gmail.com>
2024-04-17 16:27:16 +0530

    creator dashboard

---
+++ app/(dashboard)/u/[username]/_components/navbar/index.tsx
@@ -0,0 +1,11 @@
+import { Actions } from "./actions"
+import { Logo } from "./logo"
+
+export const Navbar = () => {
+    return(
+        <nav className="fixed top-0 w-full h-20 z-[40] bg-[#252731] px-2 lg:p-4 flex justify-between items-center shadow-sm">
+            <Logo />
+            <Actions />
+        </nav>
+    )
+}
